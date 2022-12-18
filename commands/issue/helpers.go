9fcd90e5f326c27f3acd88655e476666a9c80842
Aldanis Vigo <aldanisvigo@gmail.com>
2022-12-17 20:56:06 -0600

    changing structure for git_config to match the structure of go_mod

--- commands/issue/helpers.go
+++ commands/issue/helpers.go
@@ -19,27 +19,29 @@ func getBranchPrefix(ctx *runtime.Context) (branchPrefix string, err error) {
 	return
 }
 
+/*
+	Temporary Fix:
+	Gets the git repo config file relative to this path, reads the file, and extracts the repo url
+*/
 func getConfigRepoUrl() (string, error){
-	gitrepoconfiglocation := "../.git/config"
-	
-	data, err := ioutil.ReadFile(gitrepoconfiglocation)
-	if err != nil {
-		return "", err
-	}
-
-	regexpression := `(^?url).*` 
-	urlLineInFile,err := regexp.Compile(regexpression) 
-	if err != nil {
-		return "", err
-	}
+    gitrepoconfiglocation := "../.git/config"
     
-	urlmatch := urlLineInFile.FindStringSubmatch(string(data))
-	if urlmatch != nil { 
-		url := strings.Split(urlmatch[0],"url =")[1]
-		return url, nil;
-	}else{ 
-		return "", errors.New("No github URL found in the git config. Most likely not a repository.")
-	}
+    data, err := ioutil.ReadFile(gitrepoconfiglocation)
+    if err != nil {
+      return "", err
+    }
+
+    regexpression := `(^?url).*` 
+    urlLineInFile,err := regexp.Compile(regexpression) 
+    if err != nil {
+      return "", err
+    }
+    
+    urlmatch := urlLineInFile.FindStringSubmatch(string(data))
+    if urlmatch != nil {   
+      url := strings.Split(urlmatch[0],"url =")[1]
+      return url, nil;
+    }else{   
+      return "", errors.New("No github URL found in the git config. Most likely not a repository.")
+    }
 }
-
-
