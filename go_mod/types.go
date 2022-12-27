e9399514cb0fb61a125a6c52c57fb4a1c7d0e12f
Samuel Stoltenberg <sam@skelouse.com>
2022-12-27 16:53:41 -0600

    Move type tests to types_test.go

--- go_mod/types.go
+++ go_mod/types.go
@@ -1,15 +1,10 @@
 package go_mod
 
 import (
-	"github.com/taubyte/office-space/common"
 	"golang.org/x/mod/modfile"
 )
 
-var _ common.GoMod = &goMod{}
-
 type goMod struct{}
-
-var _ common.GoModFile = &goModFile{}
 
 type goModFile struct {
 	path string
---
+++ go_mod/types_test.go
@@ -0,0 +1,6 @@
+package go_mod
+
+import "github.com/taubyte/office-space/common"
+
+var _ common.GoMod = &goMod{}
+var _ common.GoModFile = &goModFile{}
