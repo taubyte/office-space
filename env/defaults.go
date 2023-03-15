a4c9c1e17b72c218cab9ec071ff3306de49e13ba
Samuel Stoltenberg <sam@skelouse.com>
2023-03-15 10:03:05 -0500

    add key

--- env/defaults.go
+++ env/defaults.go
@@ -10,4 +10,5 @@ const (
 	WorkspaceExtEnvKey       = "OFFICE_WORKSPACE_EXT"
 	WorkspaceDirectoryEnvKey = "OFFICE_WORKSPACE_DIRECTORY"
 	GitPrefixEnvKey          = "OFFICE_GIT_PREFIX"
+	NoOpenCodeEnvKey         = "OFFICE_NO_OPEN_CODE"
 )
